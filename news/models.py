5f94fe88b64d0f450d02e9768cdef7c7b9b5857d
Golshani <golshani.ma@gmail.com>
2024-03-03 08:30:24 +0330

    image Finished.

--- news/models.py
+++ news/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
-    image = models.ImageField(default='news/default.jpg')
+    image = models.ImageField(default='news/im')
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=255)
     content = models.TextField()
